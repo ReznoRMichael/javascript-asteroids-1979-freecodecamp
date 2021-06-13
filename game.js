b0963dc9dda2401fa40223566b91b91f43ba2de3
ReznoRMichael <mic3arena@gmail.com>
2021-06-13 16:40:32 +0200

    new sound hit

--- game.js
+++ game.js
@@ -39,6 +39,7 @@ var ctx = canv.getContext("2d");
 /* set up sound effects */
 var fxLaser = new Sound("sounds/laser.m4a", 5, 0.5);
 var fxExplode = new Sound("sounds/explode.m4a", 2, 0.5);
+var fxHit = new Sound("sounds/hit.m4a", 5, 0.4);
 
 // set up the game parameters
 var level, roids, ship, text, textAlpha, lives, score, scoreHigh;
